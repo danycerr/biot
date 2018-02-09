135dba7ac747c63e46dbfc3fe7cdb91129114b5f
danycerr <cerroni@mox24.mate.polimi.it>
2018-02-09 17:23:08 +0000

    non dimensional form of eq mon

--- biot_ls.hpp
+++ biot_ls.hpp
@@ -95,7 +95,7 @@ struct problem_descriptor_quad{
 	std::string INTEGRATION =       "IM_GAUSS_PARALLELEPIPED(2,6)";
     std::string SIMPLEX_INTEGRATION="IM_STRUCTURED_COMPOSITE(IM_TRIANGLE(6),6)"; 
     std::string datafilename="laplace"; 
-    int nsubdiv=20; // subdivision of the sqaured mesh
+    int nsubdiv=40; // subdivision of the sqaured mesh
     int noised =1;  // noise on mesh
     double E=1.e+4;
 	double poisson =0.0;
