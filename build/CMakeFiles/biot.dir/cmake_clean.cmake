704c4009d9d5836c4b8c447c3c39495e41fbe34d
danycerr <cerroni@mox24.mate.polimi.it>
2018-02-28 16:59:36 +0000

    trying a cmake

---
+++ build/CMakeFiles/biot.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/biot.dir/biot_ls.cpp.o"
+  "CMakeFiles/biot.dir/biot.cpp.o"
+  "CMakeFiles/biot.dir/main.cpp.o"
+  "biot.pdb"
+  "biot"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/biot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
