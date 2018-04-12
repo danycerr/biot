11f9758a0d2b9342c1bdd38516f9359f71e134a2
danycerr <cerroni@mox24.mate.polimi.it>
2018-04-12 08:16:26 +0000

    real geometry work

--- biot.hpp
+++ biot.hpp
@@ -2,28 +2,28 @@
 #define BIOT_H
 /*===========================================================================
 
- Copyright (C) 2002-2016 Yves Renard, Julien Pommier.
+  Copyright (C) 2002-2016 Yves Renard, Julien Pommier.
 
- This file is a part of GetFEM++
+  This file is a part of GetFEM++
 
- GetFEM++  is  free software;  you  can  redistribute  it  and/or modify it
- under  the  terms  of the  GNU  Lesser General Public License as published
- by  the  Free Software Foundation;  either version 3 of the License,  or
- (at your option) any later version along with the GCC Runtime Library
- Exception either version 3.1 or (at your option) any later version.
- This program  is  distributed  in  the  hope  that it will be useful,  but
- WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- or  FITNESS  FOR  A PARTICULAR PURPOSE.  See the GNU Lesser General Public
- License and GCC Runtime Library Exception for more details.
- You  should  have received a copy of the GNU Lesser General Public License
- along  with  this program;  if not, write to the Free Software Foundation,
- Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA.
+  GetFEM++  is  free software;  you  can  redistribute  it  and/or modify it
+  under  the  terms  of the  GNU  Lesser General Public License as published
+  by  the  Free Software Foundation;  either version 3 of the License,  or
+  (at your option) any later version along with the GCC Runtime Library
+  Exception either version 3.1 or (at your option) any later version.
+  This program  is  distributed  in  the  hope  that it will be useful,  but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or  FITNESS  FOR  A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+  License and GCC Runtime Library Exception for more details.
+  You  should  have received a copy of the GNU Lesser General Public License
+  along  with  this program;  if not, write to the Free Software Foundation,
+  Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA.
 
-===========================================================================*/
+  ===========================================================================*/
 
 /**@file laplacian.cpp
-   @brief Laplacian (Poisson) problem with generic assembly.
-*/
+  @brief Laplacian (Poisson) problem with generic assembly.
+  */
 
 #include "getfem/getfem_generic_assembly.h"
 #include "getfem/getfem_export.h"
@@ -36,6 +36,7 @@
 #include "gmm/gmm.h"
 
 #include "gmm/gmm_inoutput.h"
+#include "getfem/getfem_import.h"
 
 // Preconditioner
 #include "biot_precond.hpp" 
@@ -56,48 +57,48 @@ typedef std::vector<scalar_type> plain_vector;
 // scalar_type sol_f(const base_node &x) { return 10.; }
 
 struct problem_descriptor_tri{    
-    std::string MESH_TYPE =         "GT_PK(2,1)" ; // triangular elements
-    std::string FEM_TYPE_U  =         "FEM_PK(2,2)";
-    std::string FEM_TYPE_P  =         "FEM_PK(2,1)";
+	std::string MESH_TYPE =         "GT_PK(2,1)" ; // triangular elements
+	std::string FEM_TYPE_U  =         "FEM_PK(2,2)";
+	std::string FEM_TYPE_P  =         "FEM_PK(2,1)";
 	std::string INTEGRATION =       "IM_TRIANGLE(6)";
-    std::string SIMPLEX_INTEGRATION="IM_STRUCTURED_COMPOSITE(IM_TRIANGLE(6),6)"; 
-    std::string datafilename="resu/laplace"; 
-    int nsubdiv=40; // subdivision of the sqaured mesh
-    double E=1.e+10;
+	std::string SIMPLEX_INTEGRATION="IM_STRUCTURED_COMPOSITE(IM_TRIANGLE(6),6)"; 
+	std::string datafilename="resu/laplace"; 
+	int nsubdiv=10; // subdivision of the sqaured mesh
+	double E=1.e+10;
 	double poisson =0.3;
 	double mu_s = E/( 2 * ( 1 + poisson) ) ;
 	double lambda_l= E*poisson/ ( ( 1+poisson ) * (1 - 2 * poisson)) ;
 	double biot_modulus=1.e+9;
 	double k =1.e-10; //permeability
 	double alpha=1; // Biot coefficient
-    };
-    
+};
+
 struct problem_descriptor_quad{    
-    std::string MESH_TYPE =         "GT_LINEAR_QK(2)" ; // triangular elements
-    std::string FEM_TYPE_U  =         "FEM_QK(2,2)";
-    std::string FEM_TYPE_P  =         "FEM_QK(2,1)";
+	std::string MESH_TYPE =         "GT_LINEAR_QK(2)" ; // triangular elements
+	std::string FEM_TYPE_U  =         "FEM_QK(2,2)";
+	std::string FEM_TYPE_P  =         "FEM_QK(2,1)";
 	std::string INTEGRATION =       "IM_GAUSS_PARALLELEPIPED(2,6)";
-    std::string SIMPLEX_INTEGRATION="IM_STRUCTURED_COMPOSITE(IM_TRIANGLE(6),6)"; 
-    std::string datafilename="laplace"; 
-    int nsubdiv=10; // subdivision of the sqaured mesh
-    double E=1.e+4;
+	std::string SIMPLEX_INTEGRATION="IM_STRUCTURED_COMPOSITE(IM_TRIANGLE(6),6)"; 
+	std::string datafilename="laplace"; 
+	int nsubdiv=10; // subdivision of the sqaured mesh
+	double E=1.e+4;
 	double poisson =0.0;
 	double mu_s = E/( 2 * ( 1 + poisson) ) ;
 	double lambda_l= E*poisson/ ( ( 1+poisson ) * (1 - 2 * poisson)) ;
 	double biot_modulus=1.e+18;
 	double k =1.e-4; //permeability
 	double alpha=1; // Biot coefficient
-    };
+};
 
 struct problem_descriptor_quad_3d{    
-    std::string MESH_TYPE =         "GT_LINEAR_QK(3)" ; // triangular elements
-    std::string FEM_TYPE_U  =         "FEM_QK(3,2)";
-    std::string FEM_TYPE_P  =         "FEM_QK(3,1)";
+	std::string MESH_TYPE =         "GT_LINEAR_QK(3)" ; // triangular elements
+	std::string FEM_TYPE_U  =         "FEM_QK(3,2)";
+	std::string FEM_TYPE_P  =         "FEM_QK(3,1)";
 	std::string INTEGRATION =       "IM_GAUSS_PARALLELEPIPED(3,6)";
-    std::string SIMPLEX_INTEGRATION="IM_STRUCTURED_COMPOSITE(IM_TRIANGLE(6),6)"; 
-    std::string datafilename="resu/laplace"; 
-    int nsubdiv=6; // subdivision of the sqaured mesh
-    double E=1.e+10;
+	std::string SIMPLEX_INTEGRATION="IM_STRUCTURED_COMPOSITE(IM_TRIANGLE(6),6)"; 
+	std::string datafilename="resu/laplace"; 
+	int nsubdiv=6; // subdivision of the sqaured mesh
+	double E=1.e+10;
 	double poisson =0.3;
 	double mu_s = E/( 2 * ( 1 + poisson) ) ;
 	double lambda_l= E*poisson/ ( ( 1+poisson ) * (1 - 2 * poisson)) ;
@@ -106,84 +107,84 @@ struct problem_descriptor_quad_3d{
 	double alpha=1; // Biot coefficient
 	double rho_l=1000; // Biot coefficient
 	double rho_r=2200; // Biot coefficient
-    // non dimensional quantities
-    double l_ref=1;
-    double sigma_ref=1;
-    double t_ref=1;
-    double u_ref=1;
-    double p_ref=1;
-    };
-    
-    struct problem_descriptor_tetra_3d{    
-    std::string MESH_TYPE =         "GT_PK(3,1)" ; // triangular elements
-    std::string FEM_TYPE_U  =         "FEM_PK(3,2)";
-    std::string FEM_TYPE_P  =         "FEM_PK(3,1)";
+	// non dimensional quantities
+	double l_ref=1;
+	double sigma_ref=1;
+	double t_ref=1;
+	double u_ref=1;
+	double p_ref=1;
+};
+
+struct problem_descriptor_tetra_3d{    
+	std::string MESH_TYPE =         "GT_PK(3,1)" ; // triangular elements
+	std::string FEM_TYPE_U  =         "FEM_PK(3,2)";
+	std::string FEM_TYPE_P  =         "FEM_PK(3,1)";
 	std::string INTEGRATION =       "IM_TETRAHEDRON(6)";
-    std::string SIMPLEX_INTEGRATION="IM_STRUCTURED_COMPOSITE(IM_TRIANGLE(6),6)"; 
-    std::string datafilename="resu/laplace"; 
-    int nsubdiv=6; // subdivision of the sqaured mesh
-    double E=1.e+10;
+	std::string SIMPLEX_INTEGRATION="IM_STRUCTURED_COMPOSITE(IM_TRIANGLE(6),6)"; 
+	std::string datafilename="resu/laplace"; 
+	int nsubdiv=6; // subdivision of the sqaured mesh
+	double E=1.e+10;
 	double poisson =0.3;
 	double mu_s = E/( 2 * ( 1 + poisson) ) ;
 	double lambda_l= E*poisson/ ( ( 1+poisson ) * (1 - 2 * poisson)) ;
 	double biot_modulus=1.e+9;
-	double k =1.e-12; //permeability
-	double alpha=1; // Biot coefficient
+	double k =1.e-13; //permeability
+	double alpha=1.; // Biot coefficient
 	double rho_l=1000; // Biot coefficient
 	double rho_r=2200; // Biot coefficient
-    // non dimensional quantities
-    double l_ref=1;
-    double sigma_ref=1;
-    double t_ref=1;
-    double u_ref=1;
-    double p_ref=1;
-    };
+	// non dimensional quantities
+	double l_ref=1;
+	double sigma_ref=1;
+	double t_ref=1;
+	double u_ref=1;
+	double p_ref=1;
+};
 
 //   structure for the Laplacian problem
 class biot_problem {
-    private:
-      getfem::mesh mesh;
-      getfem::mesh_im mim;      /// the integration methods
-      getfem::mesh_fem mf_u;    /// the main mesh_fem, for the displacement solution
-      getfem::mesh_fem mf_p;    /// the main mesh_fem, for the pressure solution
-      getfem::mesh_fem mf_rhs;  /// the mesh_fem for the right hand side(f(x),..)
-      // problem_descriptor_tetra_3d p_des;
-      problem_descriptor_tri p_des;
-      enum { DIRICHLET_BOUNDARY_NUM = 10, NEUMANN_BOUNDARY_NUM = 11}; // descriptor for bcs flag
-      enum { BOTTOM = 2, TOP = 1 , LEFT = 3, RIGHT =4, LEFTX = 5, RIGHTX =6}; // descriptor for zones
-      size_type N_;             /// dimension of the problem
-      
-      ///  workspace configuration parameters---------------------
-      std::vector<scalar_type> tau_, vmu_, bm_ ,lambda_, beta_,
-                               alpha_, permeability_, force_,penalty_;
-     // ---------------------------------------------------------
-     sparse_matrix_type K;                                /// iteration matrix
-     std::vector<scalar_type> U, U_old, P,                /// diplacement, disp old, pressure
-                              P_old, B, UP;               /// main unknown, and right hand side
-     sparse_matrix_type Kp, Ku;                           /// iteration matrix for fixed steres of tpreconditioner
-     std::vector<scalar_type> U_iter, P_iter, Bp, Bu;     /// main unknown, and right hand side
-     biot_precond<sparse_matrix_type> *bPR_;               /// preconditioner based on fixed stress
-     
-     /// Methods
-      void gen_bc(void);                                /// create zones for boundary conditions
-      void configure_workspace                          /// configure the workspace add constants
-          (getfem::ga_workspace & workspace,                /// the workspace
-           double dt);                                      /// timestep
-    public:
-      void assembly(double dt);                         /// assemble the monolithic iteration matrix for the problem
-      void assembly_p(double dt);                       /// assemble the iteration matrix for pressure, can be used as preconditioner
-      void assembly_u(double dt);                       /// assemble the iteration matrix for pressure, can be used as preconditioner
-      void build_fix_stress_preconditioner(double dt);
-      void solve(void);                                 /// solves the monolithic system 
-      void solve_fix_stress(double dt, int max_iter);   /// solves the system with classic fixed stress approach
-      void init(void);                                  /// initial configuration for the problem 
-      void print(int time=0);
-      biot_problem(void): mim(mesh), mf_u(mesh), mf_rhs(mesh), mf_p(mesh)
-      ,tau_(1), vmu_(1), bm_(1), lambda_(1),alpha_(1), permeability_(1), force_(1), beta_(1),penalty_(1)
-      {}
+	private:
+		getfem::mesh mesh;
+		getfem::mesh_im mim;      /// the integration methods
+		getfem::mesh_fem mf_u;    /// the main mesh_fem, for the displacement solution
+		getfem::mesh_fem mf_p;    /// the main mesh_fem, for the pressure solution
+		getfem::mesh_fem mf_rhs;  /// the mesh_fem for the right hand side(f(x),..)
+		problem_descriptor_tetra_3d p_des;
+		// problem_descriptor_tri p_des;
+		enum { DIRICHLET_BOUNDARY_NUM = 10, NEUMANN_BOUNDARY_NUM = 11}; // descriptor for bcs flag
+		enum { BOTTOM = 2, TOP = 1 , LEFT = 3, RIGHT =4, LEFTX = 5, RIGHTX =6}; // descriptor for zones
+		size_type N_;             /// dimension of the problem
+
+		///  workspace configuration parameters---------------------
+		std::vector<scalar_type> tau_, vmu_, bm_ ,lambda_, beta_,
+			alpha_, permeability_, force_,penalty_;
+		// ---------------------------------------------------------
+		sparse_matrix_type K;                                /// iteration matrix
+		std::vector<scalar_type> U, U_old, P,                /// diplacement, disp old, pressure
+			P_old, B, UP;               /// main unknown, and right hand side
+		sparse_matrix_type Kp, Ku;                           /// iteration matrix for fixed steres of tpreconditioner
+		std::vector<scalar_type> U_iter, P_iter, Bp, Bu;     /// main unknown, and right hand side
+		biot_precond<sparse_matrix_type> *bPR_;               /// preconditioner based on fixed stress
+
+		/// Methods
+		void gen_bc(void);                                /// create zones for boundary conditions
+		void configure_workspace                          /// configure the workspace add constants
+			(getfem::ga_workspace & workspace,                /// the workspace
+			 double dt);                                      /// timestep
+	public:
+		void assembly(double dt);                         /// assemble the monolithic iteration matrix for the problem
+		void assembly_p(double dt);                       /// assemble the iteration matrix for pressure, can be used as preconditioner
+		void assembly_u(double dt);                       /// assemble the iteration matrix for pressure, can be used as preconditioner
+		void build_fix_stress_preconditioner(double dt);
+		void solve(void);                                 /// solves the monolithic system 
+		void solve_fix_stress(double dt, int max_iter);   /// solves the system with classic fixed stress approach
+		void init(void);                                  /// initial configuration for the problem 
+		void print(int time=0);
+		biot_problem(void): mim(mesh), mf_u(mesh), mf_rhs(mesh), mf_p(mesh)
+				    ,tau_(1), vmu_(1), bm_(1), lambda_(1),alpha_(1), permeability_(1), force_(1), beta_(1),penalty_(1)
+	{}
 };
 
 
 
- 
+
 #endif // LAPLACE_GA_H
--- main.cpp
+++ main.cpp
@@ -1,58 +1,59 @@
 /**************************************************************************/
 /*  main program.                                                         */
 /**************************************************************************/
-#include "biot_ls.hpp"
-// #include "biot.hpp"
+// #include "biot_ls.hpp"
+#include "biot.hpp"
 
 
 int main(int argc, char *argv[]) {
 
-  GMM_SET_EXCEPTION_DEBUG; // Exceptions make a memory fault, to debug.
-  FE_ENABLE_EXCEPT;        // Enable floating point exception for Nan.
+	GMM_SET_EXCEPTION_DEBUG; // Exceptions make a memory fault, to debug.
+	FE_ENABLE_EXCEPT;        // Enable floating point exception for Nan.
 
-  try {    
-    biotls_problem p;
-    // biot_problem p;
-    p.init();
+	try {    
+		//   biotls_problem p;
+		biot_problem p;
+		p.init();
 
 
-     double dt=4e+6;
-     // p.build_fix_stress_preconditioner(dt,0);
-   //    p.assembly_p(dt,0); 
-   //    p.assembly_u(dt);
-     int n_step=80;int erosion_limit=20;
-     double time=0*dt;
-     double time_ls  =0;
-     for(int istep=0; istep<n_step; istep++)
-      {//level set
-        time=istep*dt;
-        std::cout<< "*** Time step "<< istep << "/"<<n_step<<std::endl;
-         { // level set
-             if (istep<erosion_limit){ 
-                 time_ls  =istep*dt;
-                 p.update_ls(time_ls, istep);
-                 
-                 p.build_fix_stress_preconditioner(dt,time_ls);
-                }
-            p.solve_fix_stress(dt, 2000,time_ls);
-           
-        // p.build_fix_stress_preconditioner(dt,time_ls);
-       // ===============================
-         // p.assembly(dt,time_ls);
-        //  p.solve(time_ls);
-       // ===========================
-        p.print(istep*dt,istep,time_ls);      
-        p.print_crop(istep*dt,istep,time_ls);
-        } // endl of lev_set biot
-         { // classic biot
-            //   p.solve_fix_stress(dt, 2000);
-             // p.assembly(dt);
-             // p.solve();
-             // p.print(istep);
-            }
-    }
-  }
-  GMM_STANDARD_CATCH_ERROR;
+		// double dt=1e-3;
+		double dt=1e+6;
+		// p.build_fix_stress_preconditioner(dt,0);
+		//    p.assembly_p(dt,0); 
+		//    p.assembly_u(dt);
+		int n_step=80;int erosion_limit=20;
+		double time=0*dt;
+		double time_ls  =0;
+		for(int istep=0; istep<n_step; istep++)
+ 		{//level set
+ 			time=istep*dt;
+ 			std::cout<< "*** Time step "<< istep << "/"<<n_step<<std::endl;
+ 			{ // level set
+ 		// 		if (istep<erosion_limit){ 
+ 		// 			time_ls  =istep*dt;
+ 		// 			p.update_ls(time_ls, istep);
+ 
+ 		// 			p.build_fix_stress_preconditioner(dt,time_ls);
+ 		// 		}
+ 		// 		p.solve_fix_stress(dt, 2000,time_ls);
+ 
+ 		// 		// p.build_fix_stress_preconditioner(dt,time_ls);
+ 		// 		// ===============================
+ 		// 		// p.assembly(dt,time_ls);
+ 		// 		//  p.solve(time_ls);
+ 		// 		// ===========================
+ 		// 		p.print(istep*dt,istep,time_ls);      
+ 		// 		p.print_crop(istep*dt,istep,time_ls);
+ 			} // endl of lev_set biot
+			{ // classic biot
+ 				  // p.solve_fix_stress(dt, 2000);
+ 				   p.assembly(dt);
+ 				   p.solve();
+ 				 p.print(istep);
+			}
+		}
+	}
+	GMM_STANDARD_CATCH_ERROR;
 
-  return 0; 
+	return 0; 
 }
